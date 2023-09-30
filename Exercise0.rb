6a301eb94d0e6501bd57e8e1fde133e992456c18
gabelee <gabennlee@gmail.com>
2023-09-29 17:25:32 -0700

    Complete Assignment

--- Exercise0.rb
+++ Exercise0.rb
@@ -1,2 +1,6 @@
-puts "hello world"
-#changes
+#exercise 0 - displays all square roots of numbers between 1 and 1000
+
+
+for a in 1..1001 do
+    puts "The square root of #{a} is #{Math.sqrt(a)}
+end
